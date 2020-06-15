692c2202a31746b0601e7b33deb548958566190d
Allan <allanoliveira.dev@gmail.com>
2020-06-14 22:32:37 -0300

    finished mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,48 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+const allArticles
+const articleArr = ["bootstrap", "javascript", "jquery", "node", "technology"]
+const cards = document.querySelector(".cards-container")
+function cardMaker(dataArr) {
+
+    const card = document.createElement("div")
+    card.className = "card"
+    dataArr.forEach(el => {
+        innerHTML(el, card)
+    })
+    
+    return card
+}
+
+
+
+function innerHTML(el, card) {
+    card.innerHTML += `
+            <div class="headline">${el.headline}</div>
+            <div class="author">
+                <div class="img-container">
+                    <img src=${el.authorPhoto} />
+                </div>
+            <span>By ${el.authorName}</span>
+            </div>
+        `
+}
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    // .then(response => console.log(response.data.articles))
+    .then(response => {
+        const data = response.data.articles
+        // loop throgh articles and call cardMaker for each article?
+        return data
+    })
+    .then(data => {
+        articleArr.forEach(el => {
+            const result = cardMaker(data[el])
+            cards.appendChild(result)
+        })
+    })
+    .then(result => {
+        cards.appendChild(cardMaker(result))
+    }) 
